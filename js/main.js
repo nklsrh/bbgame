583d24bd66dbc05bc241fdb64bfd1fad3d1dd183
Nikhil Suresh <nklsrh@github.com>
2012-05-29 14:59:06 +1000

    First commit! Basic layout folders.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var FPS = 60;
+var width = 512,
+    height = 384;
+
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+
+function main(){
+  canvas.width = width;
+  canvas.width = height;
+}
