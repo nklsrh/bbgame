cf6c4d252b1d878f21f3023c0b3413ed5fc9528e
Nikhil Suresh <nklsrh@github.com>
2012-06-23 17:11:37 +1000

    MILESTONE! V 0.0.7.2 TILE OPTIMISATION GET! only redraw when rotation complete

--- javascripts/game/assets.js
+++ javascripts/game/assets.js
@@ -2,9 +2,11 @@ function Assets(){
   
   this.hydrogen = new Image();
   this.tile_white = new Image();
+  this.tile_purple = new Image();
   
   this.Setup = function(){
     this.hydrogen.src = "./assets/objects/chars/player/player/hydrogen.png";
     this.tile_white.src = "./assets/objects/env/tiles/tile_white_128.png";
+    this.tile_purple.src = "./assets/objects/env/tiles/tile_purple_128.png";
   }
 }
